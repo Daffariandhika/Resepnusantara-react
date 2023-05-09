b81248015dcc8316c8dd0507520dbf3de262954e
Archanest <muhammaddaffariandhika@gmail.com>
2023-05-10 06:10:57 +0700

    Search Functionality Worked
    We achieve the search functionality that we desired as it should! cheers

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
+const PREVIOUS_SEARCHES_KEY = "previousSearches";
+const MAX_PREVIOUS_SEARCHES = 10;
+
 export default function PreviousSearches({ onSearch }) {
   const [searchTerm, setSearchTerm] = useState("");
+  const [searches, setSearches] = useState([]);
+
+  useEffect(() => {
+    const previousSearches = localStorage.getItem(PREVIOUS_SEARCHES_KEY);
+    if (previousSearches) {
+      setSearches(JSON.parse(previousSearches));
+    }
+  }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (searchTerm.trim() === "") {
+      return;
+    }
+    setSearches((prevSearches) => {
+      if (!prevSearches.includes(searchTerm.toLowerCase())) {
+        const newSearches = [...prevSearches, searchTerm.toLowerCase()];
+        if (newSearches.length > MAX_PREVIOUS_SEARCHES) {
+          newSearches.shift();
+        }
+        localStorage.setItem(
+          PREVIOUS_SEARCHES_KEY,
+          JSON.stringify(newSearches)
+        );
+        return newSearches;
+      }
+      return prevSearches;
+    });
     onSearch(searchTerm);
   };
 
   const handleInputChange = (event) => {
     setSearchTerm(event.target.value);
   };
-
-  const searches = ["gudeg", "nasi uduk"];
 
   return (
     <div className="previous-searches section">
@@ -26,9 +52,11 @@ export default function PreviousSearches({ onSearch }) {
             value={searchTerm}
             onChange={handleInputChange}
           />
-          <button className="btn" type="submit">
-            <FontAwesomeIcon icon={faSearch} />
-          </button>
+          {searchTerm.trim() !== "" && (
+            <button className="btn" type="submit">
+              <FontAwesomeIcon icon={faSearch} />
+            </button>
+          )}
         </div>
       </form>
       <h2>Previous Searches</h2>
@@ -47,3 +75,4 @@ export default function PreviousSearches({ onSearch }) {
     </div>
   );
 }
+
--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,91 +1,102 @@
-import Search from "../components/Search"
-import RecipeCard from "../components/RecipeCard"
+import { useState } from "react";
+import Search from "../components/Search";
+import RecipeCard from "../components/RecipeCard";
 
-export default function Recipes(){
-    const recipes = [
-        {
-            title: "",
-            image: "/gallery/",
-            desc: "",
-        }, 
-        {
-            title: "",
-            image: "/gallery/",
-            desc: "",
-        },
-        {
-            title: "",
-            image: "/gallery/",
-            desc: "",
-        },
-        {
-            title: "",
-            image: "/gallery/",
-            desc: "",
-        },
-        {
-            title: "",
-            image: "/gallery/",
-            desc: "",
-        },
-        {
-            title: "",
-            image: "/gallery/",
-            desc: "",
-        }, 
-        {
-            title: "",
-            image: "/gallery/",
-            desc: "",
-        },
-        {
-            title: "",
-            image: "/gallery/",
-            desc: "",
-        },
-        {
-            title: "",
-            image: "/gallery/",
-            desc: "",
-        },
-        {
-            title: "",
-            image: "/gallery/",
-            desc: "",
-        },
-        {
-            title: "",
-            image: "/gallery/",
-            desc: "",
-        },
-        {
-            title: "",
-            image: "/gallery/",
-            desc: "",
-        },
-        {
-            title: "",
-            image: "/gallery/rendang.jpg",
-            desc: "",
-        },
-        {
-            title: "Gudeg",
-            image: "/gallery/gudeg.jpg",
-            desc: "Gudeg adalah salah satu makanan tradisional khas Yogyakarta dan Jawa Tengah, Indonesia. Makanan ini terbuat dari nangka muda yang dimasak dengan berbagai bumbu rempah dan gula merah sehingga mempunyai cita rasa manis dan sedikit gurih.",
-        }
-    ].sort(() => Math.random() - 0.5)
+export default function Recipes() {
+  const [filteredRecipes, setFilteredRecipes] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const limitedRecipes = recipes.slice(0, 12)
+  const recipes = [
+    {
+      title: "Gudeg",
+      image: "/gallery/yogyakarta/gudeg.jpg",
+      desc: "",
+    },
+    {
+      title: "Klepon",
+      image: "/gallery/jawatengah/klepon.jpg",
+      desc: "",
+    },
+    {
+      title: "Nasi Tiwul",
+      image: "/gallery/jawatengah/nasi-tiwul.jpg",
+      desc: "",
+    },
+    {
+      title: "Salad Buah",
+      image: "/gallery/salad-buah.jpg",
+      desc: "",
+    },
+    {
+      title: "Sate Ayam",
+      image: "/gallery/sate-ayam.jpg",
+      desc: "",
+    },
+    {
+      title: "Sate Kambing",
+      image: "/gallery/sate-kambing.jpg",
+      desc: "",
+    },
+    {
+      title: "Cakwe",
+      image: "/gallery/cakwe.jpg",
+      desc: "",
+    },
+    {
+      title: "Rendang",
+      image: "/gallery/rendang.jpg",
+      desc: "",
+    },
+    {
+      title: "Babi Guling",
+      image: "/gallery/bali/babi-guling.jpg",
+      desc: "",
+    },
+    {
+      title: "Nasi Gurih",
+      image: "/gallery/aceh/nasi-gurih.jpg",
+      desc: "",
+    },
+    {
+      title: "Roti Cane",
+      image: "/gallery/aceh/roti-cane.jpg",
+      desc: "",
+    },
+    {
+      title: "Lempah Kuning",
+      image: "/gallery/bangkabelitung/lempah-kuning.jpg",
+      desc: "",
+    },
+    {
+      title: "Soto Aceh",
+      image: "/gallery/aceh/soto-aceh.jpg",
+      desc: "",
+    },
+    {
+      title: "Sate Matang",
+      image: "/gallery/aceh/sate-matang.jpg",
+      desc: "",
+    },
+  ].sort(() => Math.random() - 0.5)
 
-    return (
-        <div>
-            <Search />
-            <div className="recipes-container">
-                {/* <RecipeCard /> */}
-                {limitedRecipes.map((recipe, index) => (
-                    <RecipeCard key={index} recipe={recipe} />
-                ))}
-            </div>
-        </div>
-    )
+  const handleSearch = (searchTerm) => {
+    setSearchTerm(searchTerm);
+    const filtered = recipes.filter((recipe) =>
+      recipe.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredRecipes(filtered);
+  };
+
+  const limitedRecipes = filteredRecipes.length > 0 ? filteredRecipes : recipes.slice(0, 12);
+
+  return (
+    <div>
+      <Search onSearch={handleSearch} />
+      <div className="recipes-container">
+        {limitedRecipes.map((recipe, index) => (
+          <RecipeCard key={index} recipe={recipe} />
+        ))}
+      </div>
+    </div>
+  );
 }
