00bb3f0ccb9d3669a81283eeaab2340cbcda336f
daffariandhika <muhammaddaffariandhika@gmail.com>
2023-05-13 05:13:03 +0700

    added province properties

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,19 @@
+// Import necessary React components and FontAwesomeIcon search icon
 import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
+// Define constants for previous searches
 const PREVIOUS_SEARCHES_KEY = "previousSearches";
 const MAX_PREVIOUS_SEARCHES = 5;
 
+// Define a React component called PreviousSearches that takes in onSearch prop
 export default function PreviousSearches({ onSearch }) {
+  // Use the useState hook to define searchTerm and searches state variables
   const [searchTerm, setSearchTerm] = useState("");
   const [searches, setSearches] = useState([]);
 
+  // Use the useEffect hook to load previous searches from local storage
   useEffect(() => {
     const previousSearches = localStorage.getItem(PREVIOUS_SEARCHES_KEY);
     if (previousSearches) {
@@ -16,6 +21,7 @@ export default function PreviousSearches({ onSearch }) {
     }
   }, []);
 
+  // Define a function to handle the search form submission
   const handleSubmit = (event) => {
     event.preventDefault();
     if (searchTerm.trim() === "") {
@@ -35,23 +41,28 @@ export default function PreviousSearches({ onSearch }) {
       }
       return prevSearches;
     });
+    // Call the onSearch prop with the current search term
     onSearch(searchTerm);
   };
 
+  // Define a function to handle changes to the search input field
   const handleInputChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
+  // Render the search form and previous search terms
   return (
     <div className="previous-searches section">
       <form onSubmit={handleSubmit}>
         <div className="search-box">
+          {/* Bind searchTerm to the input field value and call handleInputChange when changed */}
           <input
             type="text"
             placeholder="Search ..."
             value={searchTerm}
             onChange={handleInputChange}
           />
+          {/* Show the search icon when searchTerm is not empty and call handleSubmit when clicked */}
           {searchTerm.trim() !== "" && (
             <button className="btn" type="submit">
               <FontAwesomeIcon icon={faSearch} />
@@ -59,7 +70,8 @@ export default function PreviousSearches({ onSearch }) {
           )}
         </div>
       </form>
-      <h2>Pencarian Sebelumnya</h2>
+      {/* Render a list of previous search terms */}
+      <h2>Previous Searches</h2>
       <div className="previous-searches-container">
         {searches.map((search, index) => (
           <button
@@ -73,6 +85,5 @@ export default function PreviousSearches({ onSearch }) {
         ))}
       </div>
     </div>
-  );
+  )
 }
-
