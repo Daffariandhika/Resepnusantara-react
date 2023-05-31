be968995e288034d4b8f96fb07a3130b5621fef1
daffariandhika <100449301+Daffariandhika@users.noreply.github.com>
2023-05-31 21:25:22 +0700

    added search province and backend improvement
    added feature
    - search based on province
    - backend database improvement

--- src/data/user.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  gender ENUM('male', 'female') NOT NULL
-);
